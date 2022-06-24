d4c07f0b10a6c6b0b5fd1150be67385398a50edf
denist2322 <parksomang125@gmail.com>
2022-06-24 12:49:22 +0900

    게시물 삭제

--- src/main/java/com/example/demo/controller/UsrArticleContoller.java
+++ src/main/java/com/example/demo/controller/UsrArticleContoller.java
@@ -10,6 +10,7 @@ import vo.Article;
 
 @Controller
 public class UsrArticleContoller {
+	// 인스턴스 변수 시작
 	private int articlesLastId;
 	private List<Article> articles;
 	
@@ -19,7 +20,9 @@ public class UsrArticleContoller {
 		
 		makeTestData();
 	}
+	// 인스턴스 변수 끝
 	
+	// 생성자
 	private void makeTestData() {
 		for(int i = 1; i <= 10; i++) {
 			String title = "제목 "+i;
@@ -28,7 +31,8 @@ public class UsrArticleContoller {
 		}
 		
 	}
-
+	
+	// 서비스 메서드 시작
 	public Article writeArticle(String title, String body) {
 		int id = articlesLastId + 1;
 		Article article = new Article(id,title,body);
@@ -40,6 +44,23 @@ public class UsrArticleContoller {
 		return article;
 	}
 	
+	private Article getArticle(int id) {
+		for(Article article : articles) {
+			if(article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		articles.remove(article);
+	}
+	
+	// 서비스 메서드 끝
+	
+	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -55,19 +76,22 @@ public class UsrArticleContoller {
 		return articles;
 	}
 	
-//	@RequestMapping("/usr/article/doDelete")
-//	@ResponseBody
-//	public String doDelete(int id) {
-//		
-//		if(articles.get(id)==null) {
-//			return this.id + "번글이 존재하지 않습니다."
-//		}
-//		else {
-//			articles.remove(this.id);
-//			return this.id + "번글이 삭제 되었습니다."
-//		}
-//	}
-
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		Article article = getArticle(id);
 		
+		if(article==null) {
+			return id + "번글이 존재하지 않습니다.";
+		}
+			deleteArticle(id);
+			
+			return id + "번글이 삭제 되었습니다.";
+
+	}
+
+	
+
+	// 액션 메서드 끝
 }
 
