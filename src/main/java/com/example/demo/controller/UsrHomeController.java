90b7e658966f2df53afdd0babe3f064484b05144
denist2322 <parksomang125@gmail.com>
2022-06-24 09:49:32 +0900

    각종 리턴타입 실험

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,29 +1,92 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.Data;
+import lombok.Getter;
+
 @Controller
 public class UsrHomeController {
-	private int cnt;
-	
-	public UsrHomeController() {
-		cnt = -1;
-	}
-	
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/getString")
 	@ResponseBody
-	public int getCount() {
-		return cnt;
+	public String getString() {
+		return "Hi";
 	}	
 	
-	@RequestMapping("/usr/home/doSetCount")
+	@RequestMapping("/usr/home/getInt")
 	@ResponseBody
-	public String doSetCount(int cnt) {
-		this.cnt = cnt;
-		return "count의 값이" +this.cnt+ "으로 초기화 되었습니다.";
+	public int getInt() {
+		return 1;
+	}	
+	
+	@RequestMapping("/usr/home/getFloat")
+	@ResponseBody
+	public float getFloat() {
+		return 1.5f;
+	}	
+	
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 10.5;
 	}
+	
+	@RequestMapping("/usr/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return true;
+	}
+	
+	
+	@RequestMapping("/usr/home/getCharacter")
+	@ResponseBody
+	public char getCharacter() {
+		return 'H';
+	}
+	
+	@RequestMapping("/usr/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수나이", 22);
+		return map;
+	}
+	
+	@RequestMapping("/usr/home/getList")
+	@ResponseBody
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("철수");
+		return list;
+	}
+	
+	@RequestMapping("/usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article();
+		
+		return article;
+	}
+
+@Data
+class Article{
+	@Getter
+	private int id;
+	@Getter
+	private String title;
+	
+	public Article() {
+		id = 1;
+		title = "제목1";
+	}
+}
 		
 }
 
